d6c419e2730e0941e0dd1a6ff5ef0fe91294dace
jktechs <59015411+jktechs@users.noreply.github.com>
2020-03-31 21:15:52 +0200

    Update sketch3.js

--- p5/sketch3.js
+++ p5/sketch3.js
@@ -2,7 +2,7 @@ let c;
 let d;
 let e = false;
 function setup() {
-  createCanvas(1600, 800);
+  createCanvas(1475, 685);
   c = createVector(width/2,height);
   d = createVector(width/2,height/1.2);
 }
