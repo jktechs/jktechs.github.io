91bd9615060e50b965a7aa6a77986072e6267c36
jktechs <59015411+jktechs@users.noreply.github.com>
2020-04-03 15:39:19 +0200

    Update sketch3.js

--- p5/sketch3.js
+++ p5/sketch3.js
@@ -1,7 +1,9 @@
 let c;
 let d;
 let e = false;
+let f;
 function setup() {
+  f = createInput("2");
   createCanvas(1475, 685);
   c = createVector(width/2,height);
   d = createVector(width/2,height/1.2);
@@ -15,12 +17,25 @@ function draw() {
 }
 function Cube(A,B,C,D){
   let l1 = Plane(A,B,C,D);
-  let c2 = createVector(C.x,-C.y+height);
-  let d2 = createVector(D.x,-D.y+height);
-  let l2 = Plane(A,B,c2,d2);
-  for(let i = 0;i<4;i++){
-    line(l1[i].x,l1[i].y,l2[i].x,l2[i].y)
-  }
+  let p1 = createVector(l1[1].x,l1[1].y-f.value()*100);
+  let r1 = DrawRay(B,p1);
+  let p2 = createVector(l1[2].x,l1[2].x*r1.a+r1.b);
+  
+  let r2 = DrawRay(A,p2);
+  let p3 = createVector(l1[3].x,l1[3].x*r2.a+r2.b);
+  
+  let r3 = DrawRay(B,p3);
+  let p4 = createVector(l1[0].x,l1[0].x*r3.a+r3.b);
+  
+  line(p1.x,p1.y,p2.x,p2.y);
+  line(p2.x,p2.y,p3.x,p3.y);
+  line(p3.x,p3.y,p4.x,p4.y);
+  line(p4.x,p4.y,p1.x,p1.y);
+  
+  line(p1.x,p1.y,l1[1].x,l1[1].y);
+  line(p2.x,p2.y,l1[2].x,l1[2].y);
+  line(p3.x,p3.y,l1[3].x,l1[3].y);
+  line(p4.x,p4.y,l1[0].x,l1[0].y);
 }
 function Plane(A,B,C,D){
   let l1 = DrawRay(A,C);
@@ -41,16 +56,18 @@ function DrawRay(A,B){
   let l = new Jitter(A.x,A.y,B.x,B.y);
   let p = createVector((height-l.b)/l.a,height);
   let p2 = createVector(-l.b/l.a,0);
-  strokeWeight(1);
+  //strokeWeight(1);
   //line(p2.x,p2.y,p.x,p.y);
   //strokeWeight(10);
   return l;
 
 }
 function mouseDragged() {
+  if(mouseY>10)
   c = createVector(mouseX,mouseY);
 }
 function mousePressed() {
+  if(mouseY>10)
   d = createVector(mouseX,mouseY);
 }
 
