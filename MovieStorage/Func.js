f6eec3a06d860af8abf163b1cd4a371dc3ead87c
jktechs <jjlkoppe@hotmail.com>
2020-08-11 11:42:20 +0200

    alert

--- MovieStorage/Func.js
+++ MovieStorage/Func.js
@@ -23,12 +23,12 @@
     var array = ["   the bee movie","   the lego movie","   some ather movie"];
     var newHTML = [];
     for (var i = 0; i < array.length; i++) {
-      newHTML.push('<p class="item" onclick=alertB();">' + array[i] + '</p>');
+      newHTML.push('<p class="item" onclick=selectId(i);">' + array[i] + '</p>');
     }
     $("#con").append(newHTML.join(""));
   });
-  function alertB(){
-    alert('alert');
+  function selectId(a){
+    alert('alert' + a);
   }
   function getString(){
     var pathReference = storage.ref('downt.txt');
